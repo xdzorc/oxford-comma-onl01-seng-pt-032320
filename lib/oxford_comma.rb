a36cb738fa00829006c545be4e77ee7b59ec1181
DAZUN XING <dazun.xing@gmail.com>
2020-04-01 09:17:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
   if array.length=1
     str=array.join
-    break
+    
   
   elsif array.length=2
     array[-1]="and #{array[-1]}"
     str=array.join
-    break
+    
   else
     array[-1]="and #{array[-1]}"
     str=array.join(",")
