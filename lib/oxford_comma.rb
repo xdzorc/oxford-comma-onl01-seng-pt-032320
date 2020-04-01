a1b2ea66a248f1ae247e17da55260c365970bdef
DAZUN XING <dazun.xing@gmail.com>
2020-04-01 09:17:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-  if array.length=1
+  if array.length==1
     str=array.join
     
   
-  elsif array.length=2
+  elsif array.length==2
     array[-1]="and #{array[-1]}"
     str=array.join
     
